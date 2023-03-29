79b7869e18014cde7683a07fbde0cfbbda44d4a0
aizidro <andreizidru@gmail.com>
2023-03-29 10:21:02 -0300

    appKazu

--- 2des/mobile/conversao/App.js
+++ 2des/mobile/conversao/App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [comprimento, setComprimento] = new useState(0);
+  const [corrente, setCorrente] = new useState(0);
+  const [resultado, setResultado] = new useState(0);
+  const [resultado127, setResultado127] = new useState(0);
+  const calcularBitola = () => {
+    let b1 = (1.72 * (comprimento * corrente)) / 510.4
+    setResultado(b1)
+    let b2 = (2 * (comprimento * corrente)) / 294.64
+    setResultado127(b2)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Digite  o valor do Comprimento(L) do Cabo</Text>
+      <TextInput style={styles.input} onChangeText={(val) => { setComprimento(val); console.log(comprimento) }} />
+      <Text>Digite o valor da corrente(I)</Text>
+      <TextInput style={styles.input} onChangeText={(val) => { setCorrente(val); console.log(corrente) }} />
+      <Button title="Result" onPress={() => calcularBitola()} />
+      <Text style={styles.result}> O resultado em 220V é: {Math.floor(resultado)}mm</Text>
+      <Text style={styles.result}> O resultado em 127V é: {Math.floor(resultado127)}mm</Text>
     </View>
   );
 }
@@ -13,8 +29,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5426ff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderBottomWidth: '1px',
+    borderBottomColor: '#333',
+    backgroundColor: '#e5e5e5',
+    padding: '5px',
   },
 });
