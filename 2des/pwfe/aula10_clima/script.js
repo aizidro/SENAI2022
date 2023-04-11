1675d9110f40cee6c8517992272dd1b055e9163d
aizidro <andreizidru@gmail.com>
2023-04-11 11:30:59 -0300

    previAtualizado

--- 2des/pwfe/aula10_clima/script.js
+++ 2des/pwfe/aula10_clima/script.js
@@ -1,20 +1,28 @@
 let sdlocais = document.querySelector("#locais")
 let sdclima = document.querySelector("#clima")
+let chuva = document.querySelector("#chuva")
+let umidade = document.querySelector("#umidade")
+let vento = document.querySelector("#vento")
 
 api.get('/current.json?key=f78ba735e98c4112a74115818231004&q=Jaguariuna&lang=pt')
     .then(resp => {
 
-
         console.log(resp.data)
         // criamos um paragrafo que mostra o nome da cidade e o horario
         var cidade = document.createElement("p")
+        let condicao = document.createElement("p")
         cidade.innerHTML = `${resp.data.location.name}, ${getHorario(resp.data.location)}`
-        sdlocais.appendChild(cidade)
+        condicao.innerHTML = resp.data.current.condition.text;
+        chuva.innerHTML = `chuva: ${resp.data.current.precip_in}%`;                                                                                                              
+        umidade.innerHTML = `Umidade: ${resp.data.current.humidity}%`;                                                                                                              
+        vento.innerHTML = `vento: ${resp.data.current.wind_mph}KM/H`;                                                                                                              
+        sdlocais.appendChild(cidade) 
+        sdlocais.appendChild(condicao)
 
         // Criamos um paragrafo e colocamos o estado do clima sol, nublado...
-        let clima = document.createElement("p")
-        clima.innerHTML = `${resp.data.current.condition.text}, ${resp.data.current.temp_c}°C` // resp = informa muitas informaçoes por isso utilizamos o . e a informação que queremos, ex .data.current
-        sdclima.appendChild(clima)
+        let clima = document.querySelector('#temperatura')
+        clima.innerHTML = ` ${resp.data.current.temp_c}°C` // resp = informa muitas informaçoes por isso utilizamos o . e a informação que queremos, ex .data.current
+
     })
 
 
