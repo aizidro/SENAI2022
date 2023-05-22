8369095def7b36dbf5b9d11bcd3a25a337bb8659
aizidro <andreizidru@gmail.com>
2023-05-22 11:28:29 -0300

    continuacaoProject

---
+++ 2des/pwfe/aula12/scripts/cardapio.js
@@ -0,0 +1,58 @@
+
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+
+adicionarCard();
+function adicionarCard() {
+    cardapio.forEach((element, index) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+        <div class="tituloImg">
+        <h1 id="titulo" onclick="displays(), showInformations(${index})" >${element.nome}</h1>
+        <img src="/assets/pizaIcone.png">
+        </div>
+        <div class="info">
+            <p id="descricao" >${element.descricao}</p>
+            <footer>
+                <h3 id="preco"> R$ ${element.preco}</h3>
+            </footer>
+            `
+        main.appendChild(card);
+    })
+}
+
+function displays() {
+    const window = document.querySelector('.window');
+    window.classList.toggle('hidden');
+    
+}
+
+function showInformations(indice){
+    console.log(indice)
+}
+
+function showInformations(posicao) {
+    const window = document.querySelector('.window');
+    const existingInfo = document.querySelector('.informacoes');
+    
+
+    // Remover informações existentes, se houver
+    if (existingInfo != null) {
+        window.removeChild(existingInfo);
+    }
+
+    // adicionar a classe para a próxima verificação de se ela existir
+    let informacoes = document.createElement('div');
+    informacoes.classList.add('informacoes');
+
+    // adicionar as informações da pizza clicada
+    informacoes.innerHTML = `
+        <p>ID: ${cardapio[posicao].id}</p>
+        <p>Nome: ${cardapio[posicao].nome}</p>
+        <p>Descrição: ${cardapio[posicao].descricao}</p>
+        <p>R$ ${cardapio[posicao].preco}</p>
+    `;
+    informacoes.classList.add('informacoes');
+    window.appendChild(informacoes);
+}
