1b326b06f7c05c6d0facc5c68f6a0eb0d0cd8d22
aizidro <andreizidru@gmail.com>
2023-05-26 07:54:30 -0300

    project_mola

--- Projetos/CalculoDeMolas - teste/scripts/mola.js
+++ Projetos/CalculoDeMolas - teste/scripts/mola.js
@@ -1,7 +1,7 @@
-const carga = document.querySelector('#cargaEsperada')
 function calculo() {
     const CompriTotalMola = document.querySelector('#compMola').value;
     const totalMolaCompri = document.querySelector('#totalMola').value;
+    const Carga = document.querySelector('#cargaEsperada').value;
     const resultadoCompressao = document.querySelector('#resultCompressao');
     const resultadoConstante = document.querySelector('#resultConstante');
     const compressao_mm = CompriTotalMola - totalMolaCompri
@@ -13,30 +13,26 @@ function calculo() {
     return (compressao_mm)
 }
 
-function laco1(potencia1) {
-    for (let i = 0.1; i <= 2; i += 0.05) {
-        potencia1 = i
-    }
+// function laco1() {
+//     for(let i = 0.1; i <= 2 ; i+=0.05){
+//        console.log(i.toFixed(2))
+//     }
 
-}
+// }
 // laco1(1)
 
-function laco2(potencia2) {
-    for (let i = 2; i <= 15; i += 0.5) {
-        potencia2 = i
-    }
-}
+// function laco2(){
+//     for(let i =2; i <=15; i+=0.5){
+//         console.log(i)
+//     }
+// }
 // laco2()
 
-function laco3(potencia3) {
-    for (let i = 1; i <= 20; i += 0.1) {
-        potencia3 = i
-    }
-}
-
-while (carga <= 400 || carga >= 410) {
-
-}
+// function laco3(){
+//     for(let i =1; i <=20; i+=0.1){
+//         console.log(i.toFixed(1))
+//     }
+// }
 // laco3()
 function calculo2() {
     const diametroDoFio = document.querySelector('#diametroFio').value;
@@ -60,45 +56,5 @@ function calculo2() {
     const compressao_mm = calculo();
     cargotota.innerHTML = `Carga esperada: ${(resultado * compressao_mm).toFixed(3)}`;
 }
-let parado = false
-function pararLaco() {
-    if (parado) {
-        parado = false
-        return false
-    }
-    parado = true
-    return true
-}
 
-function testaValor() {
-    const cargaTotal = document.querySelector('#cargaEsperada').value
-    let cargaTotalTeste = 0
-    let parar = pararLaco()
 
-    let potencia = 0;
-    let potencia2 = 0;
-    let potencia3 = 0;
-    let constante = 0;
-
-    for (let i = 0.1; i <= 2; i += 0.05) {
-        potencia = i
-        if (cargaTotalTeste >= 390 && cargaTotalTeste <= 410) break
-        for (let i2 = 2; i2 <= 15; i2 += 0.5) {
-            potencia2 = i2
-            if (cargaTotalTeste >= 390 && cargaTotalTeste <= 410) break
-            for (let i3 = 1; i3 <= 20; i3 += 0.1) {
-                potencia3 = i3
-                constante = recebeConstante(potencia**4, potencia2, potencia3**3)
-                cargaTotalTeste = constante * calculo()
-                console.log({ potencia1: potencia, potencia2, potencia3: potencia3 })
-                console.log({ cargaTotalTeste })
-                if (cargaTotalTeste >= 390 && cargaTotalTeste <= 410) break
-            }
-        }
-    }
-
-}
-
-function recebeConstante(potencia1, potencia2, potencia3) {
-    return Math.floor((8200 * potencia1) / 8 / potencia3 / potencia2)
-}
